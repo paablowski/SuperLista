6ecda108e15adba37221d71641d39e6a47f4604a
Pablo Felipe <p.soto08@ufromail.cl>
2018-08-16 09:53:10 -0300

    v1
    primer commit

---
+++ app/src/main/java/cl/paablo/superlista/AgregarProducto.java
@@ -0,0 +1,13 @@
+package cl.paablo.superlista;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AgregarProducto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_producto);
+    }
+}
